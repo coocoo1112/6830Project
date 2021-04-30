c55d43820724ececdc6dfbc244090b19a0855573
coocoo1112 <46492679+coocoo1112@users.noreply.github.com>
2021-04-30 16:20:24 -0400

    table stats json

--- generate_sql.py
+++ generate_sql.py
@@ -4,6 +4,7 @@ import sys
 import itertools
 import random
 import math
+import os
 
 join_types = ["right", "left", "inner", "outer"]
 
@@ -13,24 +14,50 @@ joins = {('part', 'partsupp'): (["part.p_partkey=partsupp.ps_partkey"], ['partke
             ('customer', 'nation'): (["customer.c_nationkey=nation.n_nationkey"], ['nationkey']), ('supplier', 'nation'): (["supplier.s_nationkey=nation.n_nationkey"], ['nationkey']),
             ('partsupp', 'lineitem'): (["partsupp.ps_partkey=lineitem.l_partkey", "partsupp.ps_suppkey=lineitem.l_suppkey"], ['partkey', 'suppkey']), 
             ('customer', 'orders'): (["customer.c_custkey=orders.o_custkey"], ['custkey']), ('orders', 'lineitem'): (["orders.o_orderkey=lineitem.l_orderkey"], ['orderkey']),
-            ('region', 'nation'): (["region.r_regionkey=nation.n_nationkey"], ['nationkey'])}
+            ('region', 'nation'): (["region.r_regionkey=nation.n_regionkey"], ['regionkey'])}
 
-# joins = [
-#     "part.partkey=partsupp.partkey", "supplier.suppkey=partsupp.suppkey",
-#     "customer.nationkey=nation.nationkey",
-#     "supplier.nationkey=nation.nationkey", "partsupp.partkey=lineitem.partkey",
-#     "partsupp.suppkey=lineitem.suppkey", "customer.custkey=orders.custkey",
-#     "orders.orderkey=lineitem.orderkey", "region.regionkey=nation.nationkey"
-# ]
+
 tables_query = "SELECT tablename FROM pg_catalog.pg_tables \
-    WHERE schemaname != 'pg_catalog' AND \
-    schemaname != 'information_schema';"
+        WHERE schemaname != 'pg_catalog' AND \
+        schemaname != 'information_schema';"
 
 columns_query = "SELECT column_name FROM information_schema.columns\
     WHERE table_name = '{}';"
 
 stats_get = "select histogram_bounds, most_common_vals, tablename, attname FROM pg_stats\
     where tablename in {}"
+
+def get_table_sizes(tables):
+    size_get = "select count(*) FROM {}"
+    size_dict = {}
+    for table in tables:
+        size = run_query(size_get.format(table))[0][0]
+        size_dict[table] = size
+    return size_dict
+
+def get_all_stats(tables):
+    stats_get = "select tablename, attname, histogram_bounds, most_common_vals, avg_width, null_frac, n_distinct, correlation  FROM pg_stats\
+        where tablename in {}"
+    stats = run_query(stats_get.format(tuple(tables)))
+    size_dict = get_table_sizes(tables)
+    total_stats = {}
+    for stat in stats:
+        table = stat[0]
+        column = stat[1]
+        hist_bounds = stat[2]
+        common_vals = stat[3]
+        avg_width = stat[4]
+        null_frac = stat[5]
+        n_distinct = stat[6]
+        correlation = stat[7]
+        size = size_dict[table]
+        total_stats.setdefault(table, {})
+        total_stats[table].setdefault(column, {})
+        total_stats[table][column] = {'hist_bounds': hist_bounds, 'common_vals': common_vals, 'avg_width': avg_width, 'null_frac': null_frac,
+                                        'n_distinct': n_distinct, 'correlation': correlation, 'rows': size}
+    return total_stats
+    
+
 
 tables = [i[0] for i in run_query(tables_query)]
 table_columns = {}
@@ -82,10 +109,10 @@ def get_percentiles(table, column):
         return rough_percent, percentiles, min_val, max_val   
     else:
         #print(table, column, "none")
-        return None
+        return None, None, None, None
 
 def findsubsets(s, n):
-    print(n, list(map(set, itertools.combinations(s, n))))
+    #print(n, list(map(set, itertools.combinations(s, n))))
     return list(map(set, itertools.combinations(s, n)))
 
 def get_column_subsets(columns):
@@ -107,7 +134,7 @@ def generate_selects(table_columns):
     sqls = {}
     for table in table_columns:
         sqls[table] = []
-        columns = table_columns[table]
+        columns = table_columns[table] 
         subsets = get_column_subsets(columns)
         for n in subsets:
             for subset in subsets[n]:
@@ -118,10 +145,12 @@ def generate_selects(table_columns):
 def generate_filters(table_columns):
     sqls = []
     base = "select {} from {}\
-            where {} > {}"
+            where {} > {};"
     for table in table_columns:
         for column in table_columns[table]:
             percent_inc, percentiles_table, min_val, max_val = get_percentiles(table, column)
+            if percentiles_table is None:
+                continue
             for val in percentiles_table:
                 sqls.append(base.format(column, table, column, val))
     return sqls
@@ -131,9 +160,9 @@ def generate_joins():
     sqls = []
     base_filter = "Select * from {}\
         {} join {} on {}\
-        where {}.{} > {} and {}.{} > {}"
+        where {}.{} > {} and {}.{} > {};"
     base_no_filter = "Select * from {}\
-        {} join {} on {}"
+        {} join {} on {};"
     for join in joins:
         actual_join, columns_involved = joins[join]
         for i in range(len(actual_join)):
@@ -162,21 +191,38 @@ def generate_joins():
 
 
 if __name__ =="__main__":
-    for table in stats_dict:
-        for column in stats_dict[table]:
-            print(table, column, get_percentiles(table,column))
+
+    total_sqls = []
+    table_stats = get_all_stats(tables)
+    #json_stats = json.loads(table_stats)
+    print("done")
+
+    folder = "{}/{}".format(os.getcwd(), "json")
+    with open("table_stats.json", "w") as f:
+            json.dump(table_stats, f, indent=4)
+    
+
+
+    # print("start")
+    # test = generate_selects(table_columns)
+    # print([(i, len(test[i])) for i in test])
+    # test2 = generate_joins()
+    # print("step2", len(test2))
+    # test3 = generate_filters(table_columns)
+    # print("finished", len(test3))
+    # for i in test:
+    #     total_sqls += test[i]
+    #     print(random.choice(test[i]))
+
+    # total_sqls += test2
+    # total_sqls += test3
+    # print(random.choice(test2))
+    # print(random.choice(test3))
+
+    
     # test = get_percentiles("nation", "n_nationkey")
     # print("test")
     # print(type(test))
     # print(test)
     #print(get_column_subsets(["col1", "col2", "col3", "col4", "col5", "col6"]))
     #print("\n".join(generate_selects(table_columns)))
-
-
-    
-
-
-
-
-
-

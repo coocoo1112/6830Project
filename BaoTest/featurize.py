658fb2f9bb78873c51f48d379ba3a1f5d7f7011c
coocoo1112 <46492679+coocoo1112@users.noreply.github.com>
2021-05-15 10:38:24 -0400

    neo tree featurizer

--- BaoTest/featurize.py
+++ BaoTest/featurize.py
@@ -108,8 +108,8 @@ class NeoTreeBuilder:
             new_arr = np.zeros(len(left_scan))
             for i in range(len(left_scan)):
                 new_arr[i] = 1 if left_scan[i] == 1 or right_scan[i] == 1 else 0
-            print(my_vec)
-            print(new_arr)
+            # print(my_vec)
+            # print(new_arr)
 
             my_vec = np.concatenate((query_encoding, np.concatenate((my_vec[len(query_encoding):len(query_encoding)+len(JOIN_TYPES)], new_arr))))
             return (my_vec, left, right)
@@ -318,6 +318,26 @@ class TreeFeaturizer:
     def transform(self, trees):
         for t in trees:
             _attach_buf_data(t)
+        return [self.__tree_builder.plan_to_feature_tree(x["Plan"]) for x in trees]
+
+    def num_operators(self):
+        return len(ALL_TYPES)
+
+
+class NeoTreeFeaturizer:
+    def __init__(self):
+        self.__tree_builder = None
+
+    def fit(self, trees):
+        # for t in trees:
+        #     _attach_buf_data(t)
+        # all_rels = get_all_relations(trees)
+        # stats_extractor = get_plan_stats(trees)
+        self.__tree_builder = NeoTreeBuilder()
+
+    def transform(self, trees):
+        # for t in trees:
+        #     _attach_buf_data(t)
         return [self.__tree_builder.plan_to_feature_tree(x["Plan"]) for x in trees]
 
     def num_operators(self):

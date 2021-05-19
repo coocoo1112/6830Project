c1f2618169e1ea97e832d910800e17d7900e7338
syang98 <syang98@mit.edu>
2021-05-19 00:53:27 -0400

    Progress bar done

--- BaoTest/train.py
+++ BaoTest/train.py
@@ -10,6 +10,13 @@ from sklearn.model_selection import KFold
 import argparse
 import math
 import torch
+from RDS_query import run_query
+import time
+from threading import Thread
+from tqdm import tqdm
+from time import sleep
+import queue
+
 
 class BaoTrainingException(Exception):
     pass
@@ -46,6 +53,9 @@ def train(verbose=True):
         size = math.floor(.1*get_size_vocab(sentences))
         w2v = Word2Vec(sentences, window=20, workers=16, vector_size = size)
         shape = get_shape_vector(sentences, w2v)  
+        
+         
+        w2v.save(args.save_word2vec)
     reg = model.BaoRegression(have_cache_data=False, verbose=verbose, neo=args.e, word2vec=w2v, shape=shape)
     #print(y)
     # print("1")
@@ -73,8 +83,76 @@ def train(verbose=True):
     torch.save(reg.stat_dict(), args.m)
     return reg
 
+
+def get_explain_output(query):
+    """
+    :query str the SQL query
+    :return the query and the explain analyze output
+    """
+    try:
+        return query, run_query(query)[0][0][0]
+    except:
+        return query, None
+
+def target(query, queue):
+    start= int(round(time.time() * 1000))
+    try:
+        run_query(query)
+    except:
+        raise RuntimeError("error running the query")
+
+    queue.put(f"{int(round(time.time() * 1000))-start} ms total for the query")
+    return
 def progress_bar():
-    reg = model.BaoRegression(have_cache_data=False, verbose=verbose, neo=args.e, word2vec=w2v, shape=shape)
+    shape = None
+    w2v = None
+    if args.w:
+        w2v = Word2Vec.load(args.save_word2vec)
+        for word in w2v.wv:
+            shape = word.shape
+            break
+
+    reg = model.BaoRegression(have_cache_data=False, verbose=True, neo=args.e, word2vec=w2v, shape=shape)
+    reg.load_state_dict(torch.load(args.m))
+    query = f"EXPLAIN (COSTS true, FORMAT json, BUFFERS true) {args.q}"
+    q, output = get_explain_output(query)
+    predicted = reg.predict([output])
+    predY = [pred[0] for pred in predicted][0]
+    q = queue.Queue()
+    i = 1
+    started = False
+    start = int(round(time.time() * 1000))
+    t = Thread(target=target, args=(1,q))
+
+    # eli_count = " "*10000
+    #elapsed = int(round(time.time() * 1000))-start
+    t.start()
+    # print("WHAT?")
+    over_predict = False
+    total = math.ceil(predY/1000)
+    with tqdm(total=total, position=0, leave=True) as pbar:
+        for i in tqdm(range(total), position=0, leave=True):         
+            elapsed = int(round(time.time() * 1000))-start
+            if not t.is_alive():
+               over_predict = True
+               pbar.close()
+               break
+            if elapsed >= 5000:
+                pbar.close()
+                break
+            sleep(1)
+  
+    if over_predict:
+        print(f"over prediction by {10000-elapsed} ms")
+        t.join()
+        print(q.get())
+    else:
+        print(f"under prediction by {elapsed - 10000} ms")  
+        t.join()
+        print(q.get()) 
+
+    return
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -88,14 +166,18 @@ if __name__ == "__main__":
     parser.add_argument("--c", type=str, default="result_v1.csv", required=False)
     parser.add_argument("--s",type=int, default=20, required=False)
     #path to save model and path to load
+    #for convolutional tree
     parser.add_argument("--m", type=str, required=True)
     #if we are training or doing a progress bar
     parser.add_argument("--p", type=lambda x: True if x=='True' else False, default=False, required=False)
-    
+    parser.add_argument("--save_word2vec", type=str, default="", required=False)
+    parser.add_argument("--q", type=str, default='', required=False)
     args = parser.parse_args()
     if not args.p:
         train()
     else:
-        pass
+        progress_bar()
+    
 
+    
 
